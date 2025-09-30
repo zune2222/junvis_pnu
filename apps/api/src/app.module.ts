d53aa9ff81068dce7a6f62c8f87ccd2f40feeee4
Koty <koty08@naver.com>
2025-09-30 17:12:25 +0900

    feat: 대중교통 길찾기 api, 실시간 위치 api 추가

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -4,6 +4,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { AuthModule } from './auth/auth.module';
+import { TransportModule } from './transport/transport.module';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { AuthModule } from './auth/auth.module';
     }),
     DatabaseModule,
     AuthModule,
+    TransportModule,
   ],
   controllers: [AppController],
   providers: [AppService],
